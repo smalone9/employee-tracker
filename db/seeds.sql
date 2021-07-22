07b694de27cee8a31fe31939f77af6cac2eee4b5
Samantha Malone <sammac99@gmail.com>
2021-07-22 15:29:21 -0700

    adeed to schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,3 +11,10 @@ VALUES
 (2, "Safety Officer", 120000, 3),
 (1, "Mayor", 1000000, 7),
 (1, "Teacher", 12000, 20);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+(1, "Apu", "Nahasapeemapetilon", 1, 71),
+(2, "Homer", "Simpson", 67, 2),
+(1, "Joseph", "Quimby", 42, 6),
+(1, "Edna", "Krabappel", 12000, 20);
