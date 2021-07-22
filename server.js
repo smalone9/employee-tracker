104ca718167fcf0687cf8c51e7a3631493320085
Samantha Malone <sammac99@gmail.com>
2021-07-22 10:27:59 -0700

    added middleware

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const checkServer = require("./utils/checkServer");
+
+// middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
