c9603abf66581278a54ac001e99c9c34e8f0e3da
Samantha Malone <sammac99@gmail.com>
2021-07-24 13:23:07 -0700

    fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,19 +2,19 @@ INSERT INTO department (id, name)
 VALUES
 (1, "Kwick-e-Mart"),
 (2, "Nuclear Power Plant"),
-(1, "Springfield Government"),
-(1, "Springfield Elementray School");
+(3, "Springfield Government"),
+(4, "Springfield Elementray School");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES
 (1, "Manager", 30000, 15),
 (2, "Safety Officer", 120000, 3),
-(1, "Mayor", 1000000, 7),
-(1, "Teacher", 12000, 20);
+(3, "Mayor", 1000000, 7),
+(4, "Teacher", 12000, 20);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
 (1, "Apu", "Nahasapeemapetilon", 1, 71),
 (2, "Homer", "Simpson", 67, 2),
-(1, "Joseph", "Quimby", 42, 6),
-(1, "Edna", "Krabappel", 12000, 20);
+(3, "Joseph", "Quimby", 42, 6),
+(4, "Edna", "Krabappel", 12000, 20);
